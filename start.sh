88dc1dfc9f536e4b9f2ede4ea9e9b413f1003006
mrnoobx <154804791+mrnoobx@users.noreply.github.com>
2025-06-01 01:59:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-echo "🚀 Starting aria2c..."
-aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --daemon=true
+##!/bin/bash
 
-echo "🤖 Starting Telegram bot..."
-node bot.js
+echo "🔧 Starting aria2c..."
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all=true --dir=/app/downloads --continue=true --max-connection-per-server=5 --rpc-listen-port=6800 --max-concurrent-downloads=5 --daemon=true
+
+echo "🤖 Starting the bot..."
+python3 bot.py
